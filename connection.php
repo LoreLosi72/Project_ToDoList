f609c3c428d7229701ad9873817c5f74bc6abf32
user <user@localhost>
2020-11-19 19:56:03 +0100

    Modificato il codice di connessione al database

--- connection.php
+++ connection.php
@@ -5,18 +5,11 @@ $password = '';
 $nomedb = 'agenda';
 $connection = mysqli_connect($host,$user,$password,$nomedb);
 
-if($connection->connect_error)
+$file = fopen('Appuntamenti.csv','r');
+while(($dati = fgetcsv($file,100,","))!== FALSE)
 {
-    die("Connessione non riuscita: ".$connection->connect_error);
-}else
-{
-    $file = fopen('Appuntamenti.csv','r');
-    while(($dati = fgetcsv($file,100,","))!== FALSE)
-    {
-        $sql="insert into impegni(Id_Impegni,Nome, Descrizione, Data_impegno,Orario_impegno) values(NULL,'$dati[1]', NULL, '$dati[2]', '$dati[3]')";
-	    mysqli_query($connection,$sql);
-    }
-    fclose($file);
+    $sql="insert into impegni(Id_Impegni,Nome, Descrizione, Data_impegno,Orario_impegno) values(NULL,'$dati[1]', NULL, '$dati[2]', '$dati[3]')";
+	mysqli_query($connection,$sql);
 }
-mysqli_close($connection);
+fclose($file);
 ?>
