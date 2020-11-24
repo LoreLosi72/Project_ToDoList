5fbf859a9127bbf81a6f784b0374ebe59ee790c0
user <user@localhost>
2020-11-24 21:56:14 +0100

    modifica del codice di connessione

--- connection.php
+++ connection.php
@@ -1,15 +1,16 @@
 <?php
 $host = 'localhost';
-$user = 'agenda';
+$user = 'calendario';
 $password = '';
-$nomedb = 'agenda';
+$nomedb = 'calendario';
 $connection = mysqli_connect($host,$user,$password,$nomedb);
 
-$file = fopen('Appuntamenti.csv','r');
+/*$file = fopen('Appuntamenti.csv','r');
 while(($dati = fgetcsv($file,100,","))!== FALSE)
 {
     $sql="insert into impegni(Id_Impegni,Nome, Descrizione, Data_impegno,Orario_impegno) values(NULL,'$dati[1]', NULL, '$dati[2]', '$dati[3]')";
 	mysqli_query($connection,$sql);
 }
 fclose($file);
+*/
 ?>
