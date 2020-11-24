6d7bb866487d4697974bd4f6b2cb73f7c0d54603
user <user@localhost>
2020-11-24 21:58:29 +0100

    Creazione del file con collegamento al css e visualizzare la funzione genera calendario

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'function.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+<title>CALENDARIO</title>
+<meta charset="utf-8">
+<link rel ="stylesheet" href="style.css">
+</head>
+
+<body>
+	<div id="calendar_div">
+		<?php echo Genera_Calendario(); ?>
+	</div>
+</body>
+</html>
